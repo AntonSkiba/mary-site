6d9fb2bc6b90e63f434e7d493c5141ddf4edae2c
Anton Skiba <anton.skiba@mail.ru>
2026-04-16 22:16:23 +0300

    new info

--- client/App.tsx
+++ client/App.tsx
@@ -1,18 +1,5 @@
-import {useCallback} from 'react';
-
 function App() {
-  const onClick = useCallback(() => {
-    fetch('http://localhost:5000/contact', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: 'Anton',
-        message: 'Hello'
-      })
-    });
-  }, []);
+
   return (
     <div>
       <div>Инфа</div>
